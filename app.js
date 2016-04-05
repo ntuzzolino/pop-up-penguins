d17ca17237cbbf89b271555efc9920e0ad252d28
nik tuzzolino <niktuzzolino@gmail.com>
2016-04-05 14:42:56 -0600

    trying to fix

--- app.js
+++ app.js
@@ -1,6 +1,16 @@
+var document = ["gameholder"]
+
 $(document).ready( function() {
     //This code will run after your page loads
     $(".yeti").mousedown(function() {
         alert("Oh no!");
     });
+    
+    var classes = [".penguin1",".penguin2",".penguin3",".penguin4",".penguin5",".penguin6","penguin7",".peguin8",".yeti"];
+    
+    $('#gameholder').append()
+
 });
+
+
+
